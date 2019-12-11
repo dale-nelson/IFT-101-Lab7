970157ce10b76b76be32d16f811e29a63294f77e
dale-nelson <codingdev.dale@gmail.com>
2019-12-10 20:51:01 -0700

    Don's magic

--- Lab7P1.py
+++ Lab7P1.py
@@ -1,24 +1,32 @@
+import os
+
+
 def textReading():
     #read_file = open("C:\\Users\\piano\\OneDrive\\Documents\\Python\\Lab07\\learning_python.txt")
-    read_file = open("/Users/dale_nelson/Documents/Python/Lab07/learning_python.txt","r")
+    read_file = open(f"{os.environ['HOME']}/Documents/Python/Lab07/learning_python.txt", "r")
     return read_file
+
 
 def forLoopReading():
     for line in textReading():
         print(line.strip())
     return
 
+
 def listReading():
     read_list = textReading().readlines()
     return read_list
+
 
 def reverseList():
     tmp_var = listReading()
     backwards_list = tmp_var[::-1]
     return backwards_list
 
+
 def multiplyList():
     return listReading() * 3
+
 
 def printEverything():
     print(textReading().read())
@@ -26,5 +34,6 @@ def printEverything():
     print(str(reverseList()) + "\n")
     print(multiplyList())
     return
+
 
 printEverything()
